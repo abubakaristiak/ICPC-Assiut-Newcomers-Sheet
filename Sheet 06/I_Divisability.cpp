a7df49ce11132c49fc2918ce9a19ce728eb53c80
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-11 01:59:13 +0600

    added new problems

---
+++ Sheet 06/I_Divisability.cpp
@@ -0,0 +1,54 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-11 || 01:45:48
+ * * * * File    : I_Divisability.cpp
+*/
+
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll sum(ll num) {
+    return num*(num+1)/2;
+}
+void solve()
+{
+    ll a,b,x; cin >> a >> b >> x;
+    ll mx=max(a,b);
+    ll mn=min(a,b);
+    ll res=(sum(mx/x)*x)-(sum((mn-1)/x)*x);
+    cout << res << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t=1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah---(Ramadan Kareem)
+}
